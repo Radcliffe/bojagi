31137c4613cf0c5521637722d3bd002dbefed46f
David Radcliffe <dradcliffe@gmail.com>
2014-08-01 22:22:00 -0500

    Here goes nothing

--- server.js
+++ server.js
@@ -33,18 +33,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB configuration
 var mongojs = require('mongojs');
-var connection_string = '127.0.0.1:27017/bojagi';
+// var connection_string = '127.0.0.1:27017/bojagi';
+var db_name = 'bojagi';
 
 // if OPENSHIFT env variables are present, use the available connection info:
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
-}
+// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+//  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+//  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+//  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+//  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+//  process.env.OPENSHIFT_APP_NAME;
+//}
 
-var db = mongojs(connection_string, ['levels', 'counters']);
+//provide a sensible default for local development
+var mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+var db = mongojs(mongodb_connection_string, ['levels', 'counters']);
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
