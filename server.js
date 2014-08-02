16676f62a9ba1f703709a0589351fc80617d4d69
David Radcliffe <dradcliffe@gmail.com>
2014-08-01 21:21:46 -0500

    Specify IP address

--- server.js
+++ server.js
@@ -97,8 +97,9 @@ var debug = require('debug')('my-application');
 // var app = require('../app');
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), ipaddress, function() {
   debug('Express server listening on port ' + server.address().port);
 });
 
